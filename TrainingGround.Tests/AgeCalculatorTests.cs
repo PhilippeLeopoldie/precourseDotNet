15523c88b5b5484523bd87d7165c06888afba882
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2022-12-01 14:14:46 +0100

    first test

---
+++ TrainingGround.Tests/AgeCalculatorTests.cs
@@ -0,0 +1,30 @@
+namespace TrainingGround.Tests;
+
+public class AgeCalculatorTests
+{
+    private AgeCalculator _calculator;
+    public AgeCalculatorTests()
+    {
+        _calculator = new AgeCalculator();
+    }
+
+    [Fact]
+    public void sommeone_born_1972_is_50_in_2022()
+    {
+        // act
+        var age= _calculator.GetAge(1972, 2022);
+
+        // assert
+        Assert.Equal(50, age);
+    }
+
+    [Fact]
+    public void someone_born_in_2022_is_0_in_2002()
+    {
+        //Act
+        var age = _calculator.GetAge(2022, 2022);
+
+        // Assert
+        Assert.Equal(0, age);
+    }
+}
--- TrainingGround/AgeCalculator.cs
+++ TrainingGround/AgeCalculator.cs
@@ -1,5 +1,9 @@
 ﻿namespace TrainingGround;
 public class AgeCalculator
 {
+  public int GetAge (int birthdYear, int currentYear)
+  {
+    return currentYear - birthdYear;
+  }
 
 }
