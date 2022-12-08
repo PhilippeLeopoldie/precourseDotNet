c0f7d5c71d5de461552d2861fc67a8229cde91e7
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2022-12-08 23:41:34 +0100

    Loop

--- TrainingGround.Tests/LoopTests.cs
+++ TrainingGround.Tests/LoopTests.cs
@@ -39,6 +39,45 @@ public class LoopTests
     Assert.Equal(i + 1, currentValueInTheLoop);
     }
   }
+  [Fact]
+  public void for_each()
+  {
+    // arrange
+    var ints = new int[] {1,2,3,4,5};
+
+    // act
+    foreach(var currentValueInTheLoop in ints)
+    {
+     // Console.WriteLine($"i is now '{i}'");
+      Console.WriteLine($"currentValueInTheLoop is now '{currentValueInTheLoop}'");
+    
+    // assert
+    Assert.IsType<int>(currentValueInTheLoop);
+    }
+  }
+  [Fact]
+  void for_each_loop_over_adresses()
+  {
+    // assert
+    var emp = new Employee("phil", "my-id");
+    emp.Addresses.Add(new Address(){Street="myStreet1", StreetNo = 1});
+    emp.Addresses.Add(new Address(){Street="myStreet2", StreetNo = 2});
+    emp.Addresses.Add(new Address(){Street="myStreet3", StreetNo = 3});
+    
+
+    // act
+    foreach(var oneAdress in emp.Addresses)
+    {
+      Console.WriteLine($"Address street: {oneAdress.Street}, streetNo: {oneAdress.StreetNo}");
+
+      // assert
+    Assert.IsType<List<Address>>(emp.Addresses);
+    Assert.IsType<Address>(oneAdress);
+    }
+
+    
+
+  }
 
   
   
