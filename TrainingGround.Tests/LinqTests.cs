4ec69d9a1d0a08d93a36fe4aecc36b9a172616a5
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2022-12-11 15:20:45 +0100

    SELECT

--- TrainingGround.Tests/LinqTests.cs
+++ TrainingGround.Tests/LinqTests.cs
@@ -91,6 +91,32 @@ public class LinqTests
     Assert.Equal("Aaaron", allPeopleNameLongerThan4.First().Name);
     Assert.Equal("Ceasar", allPeopleNameLongerThan4[1].Name);
   }
+
+  [Fact]
+  public void filter_poeple_by_age_only_names()
+  {
+    // arrange
+    var people = new List<Person> 
+    {
+      new Person("Aaaron"),
+      new Person("Bea"),
+      new Person("Ceasar"),
+      new Person("Dave")
+
+    };
+
+    // act
+    var allPeopleNameLongerThan4 = people
+    .Where(p => p.Name.Length >4)
+    .Select(p => p.Name)
+    .ToList();
+
+    // assert
+    Assert.Equal(2, allPeopleNameLongerThan4.Count);
+    Assert.Equal(true, allPeopleNameLongerThan4.Any(p => p =="Aaaron"));
+    Assert.Equal("Aaaron", allPeopleNameLongerThan4.First());
+    Assert.Equal("Ceasar", allPeopleNameLongerThan4[1]);
+  }
   
   
 
